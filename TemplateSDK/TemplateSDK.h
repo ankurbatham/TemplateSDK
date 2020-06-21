8974a675c9573d40e27773482a3b17386ee02e09
Ankur Batham <bathamankur001@gmail.com>
2020-06-21 13:42:38 +0530

    first commit

---
+++ TemplateSDK/TemplateSDK.h
@@ -0,0 +1,19 @@
+//
+//  TemplateSDK.h
+//  TemplateSDK
+//
+//  Created by Ankur Batham on 21/06/20.
+//  Copyright © 2020 Ankur Batham. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TemplateSDK.
+FOUNDATION_EXPORT double TemplateSDKVersionNumber;
+
+//! Project version string for TemplateSDK.
+FOUNDATION_EXPORT const unsigned char TemplateSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TemplateSDK/PublicHeader.h>
+
+
